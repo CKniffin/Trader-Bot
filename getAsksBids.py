425722c17fe44ed0163d320c59359ebc4e85ba7d
CKniffin <cakniffin1990@aol.com>
2017-08-25 07:25:56 -0700

    Changed output to numpy matrices and added comments

--- getAsksBids.py
+++ getAsksBids.py
@@ -1,21 +1,34 @@
+import numpy as np
+# Returns list of current asks with volume
 def getAsks(pc,prod):
+    # Get order book from GDAX
     order_book = pc.get_product_order_book(prod, level=3)
+    # Get asks from order book
     asks = order_book['asks']
+    # Create output list
     asksOut = [[float(asks[0][0]),float(asks[0][1])]]
+    # Traverse asks, group listings with same price and append to list
     for i in range(1,len(asks)):
         if float(asks[i][0]) == asksOut[-1][0]:
             asksOut[-1][1] += float(asks[i][1])
         else:
             asksOut.append([float(asks[i][0]),float(asks[i][1])])
-    return asksOut
+    # Output as numpy matrix
+    return np.matrix(asksOut)
 
+# Returns list of current bids with volume
 def getBids(pc,prod):
+    # Get order book from GDAX
     order_book = pc.get_product_order_book(prod, level=3)
+    # Get bids from order book
     bids = order_book['bids']
+    # Create output list
     bidsOut = [[float(bids[0][0]),float(bids[0][1])]]
+    # Travers bids, group listings with same price and append to list
     for i in range(1, len(bids)):
         if float(bids[i][0]) == bidsOut[-1][0]:
             bidsOut[-1][1] += float(bids[i][1])
         else:
             bidsOut.append([float(bids[i][0]), float(bids[i][1])])
-    return bidsOut
+    # Output as numpy matrix
+    return np.matrix(bidsOut)
--- getHistory.py
+++ getHistory.py
@@ -1,17 +1,27 @@
 import datetime
+import numpy as np
+# Returns historic close prices and volumes for a given time and granularity
 def getHistory(pc,delta,gran,prod):
+    # Current time
     endTime = datetime.datetime.utcnow()
+    # Interval start time
     startTime = endTime - datetime.timedelta(minutes=delta)
+    # Get data from GDAX
     hist = pc.get_product_historic_rates(prod, start=startTime, end=endTime, granularity=gran)
+    # Time stamp of first value in list
     startTime = hist[-1][0]
+    # Create output lists
     dt = []
     close = []
     vol = []
+    # Traverse history data and append to lists
     for i in range(len(hist)):
         dt.append(hist[i][0]-startTime)
         close.append(hist[i][4])
         vol.append(hist[i][5])
+    # Reverse lists for a more intuitive output
     dt.reverse()
     close.reverse()
     vol.reverse()
-    return [dt,close,vol]
+    # Output as numpy array
+    return np.matrix([dt,close,vol])
