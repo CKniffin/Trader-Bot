d71d6bcc69434721a01221168a656e24a332a902
unknown <CKniffin@users.noreply.github.com>
2017-09-03 20:53:37 -0700

    Added login and buy/sell examples to test file

--- APITest.py
+++ APITest.py
@@ -1,8 +1,7 @@
 import gdax
-import iso8601
-import datetime
 from getAsksBids import getAsks, getBids
 from getHistory import getHistory
+from getPriceRatios import getPriceRatios
 
 pc = gdax.PublicClient()
 last_trades = pc.get_product_trades('ETH-USD')
@@ -11,5 +10,18 @@ prods = pc.get_products()
 hist  = getHistory(pc,5,10,'ETH-USD')
 asks = getAsks(pc,'ETH-USD')
 bids = getBids(pc,'ETH-USD')
+price_ratios = getPriceRatios(pc)
+f = open('key.txt','r')
+
+key = f.readline()
+key = key[:-1]
+secret = f.readline()
+pw  = raw_input('Enter GDAX password')
+
+
+authclient = gdax.AuthenticatedClient(key,secret,pw,'https://api-public.sandbox.gdax.com')
+sell = authclient.sell(size='1',price='50000',side='sell',product_id='BTC-USD')
+buy = authclient.buy(size='1',price='50',side='buy',product_id='BTC-USD')
+
 
 1+1
